580eefbce0f366239ad38073be96e085f1fddc28
Marton Kaufmann <kaufmann.marton@protonmail.com>
2023-09-22 00:38:06 +0200

    Some stuff

--- init.lua
+++ init.lua
@@ -18,6 +18,8 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup("plugins")
 
 -- Customize keymap
+vim.keymap.set("n", "<leader>of", ":browse oldfiles", { silent = true })
+
 local telescope = require('telescope.builtin')
 vim.keymap.set("n", "<leader>ff", telescope.find_files, {})
 vim.keymap.set("n", "<leader>fg", telescope.live_grep, {})
