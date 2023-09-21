239eb418efc1158117f5b5b55768dd06415b0d00
Marton Kaufmann <kaufmann.marton@protonmail.com>
2023-09-21 19:14:20 +0200

    Initial commit

---
+++ init.lua
@@ -0,0 +1,25 @@
+vim.g.mapleader = " "
+
+-- Setup lazy.nvim for package/plugin management
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- require("lazy").setup(plugins, opts)
+require("lazy").setup("plugins")
+
+-- Customize keymap
+local telescope = require('telescope.builtin')
+vim.keymap.set("n", "<leader>ff", telescope.find_files, {})
+vim.keymap.set("n", "<leader>fg", telescope.live_grep, {})
+vim.keymap.set("n", "<leader>fb", telescope.buffers, {})
+vim.keymap.set("n", "<leader>fh", telescope.help_tags, {})
