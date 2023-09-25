d143b9800f082f31298ce5fa16a1abe1289890ed
Marton Kaufmann <kaufmann.marton@protonmail.com>
2023-09-25 15:15:09 +0200

    Replace all single quotes

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -23,46 +23,46 @@ return {
 				-- documentation = cmp.config.window.bordered(),
 			},
 			mapping = cmp.mapping.preset.insert({
-				['<C-b>'] = cmp.mapping.scroll_docs(-4),
-				['<C-f>'] = cmp.mapping.scroll_docs(4),
-				['<C-Space>'] = cmp.mapping.complete(),
-				['<C-e>'] = cmp.mapping.abort(),
-				['<CR>'] = cmp.mapping.confirm({ select = true }),
+				["<C-b>"] = cmp.mapping.scroll_docs(-4),
+				["<C-f>"] = cmp.mapping.scroll_docs(4),
+				["<C-Space>"] = cmp.mapping.complete(),
+				["<C-e>"] = cmp.mapping.abort(),
+				["<CR>"] = cmp.mapping.confirm({ select = true }),
 			}),
 			sources = cmp.config.sources({
-				{ name = 'nvim_lsp' },
-				{ name = 'vsnip' },
+				{ name = "nvim_lsp" },
+				{ name = "vsnip" },
 			}, {
-				{ name = 'buffer' },
+				{ name = "buffer" },
 			})
 		})
 
-		cmp.setup.filetype('gitcommit', {
+		cmp.setup.filetype("gitcommit", {
 			sources = cmp.config.sources({
-				{ name = 'git' },
+				{ name = "git" },
 			}, {
-				{ name = 'buffer' },
+				{ name = "buffer" },
 			})
 		})
 
-		cmp.setup.cmdline({ '/', '?' }, {
+		cmp.setup.cmdline({ "/", "?" }, {
 			mapping = cmp.mapping.preset.cmdline(),
 			sources = {
-				{ name = 'buffer' }
+				{ name = "buffer" }
 			}
 		})
 
-		cmp.setup.cmdline(':', {
+		cmp.setup.cmdline(":", {
 			mapping = cmp.mapping.preset.cmdline(),
 			sources = cmp.config.sources({
-				{ name = 'path' }
+				{ name = "path" }
 			}, {
-				{ name = 'cmdline' }
+				{ name = "cmdline" }
 			})
 		})
 
-		local capabilities = require('cmp_nvim_lsp').default_capabilities()
-		require('lspconfig')['tsserver'].setup {
+		local capabilities = require("cmp_nvim_lsp").default_capabilities()
+		require("lspconfig")["tsserver"].setup {
 			capabilities = capabilities
 		}
 	end
--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,15 +1,15 @@
 return {
 	"neovim/nvim-lspconfig",
 	config = function()
-		local lspconfig = require('lspconfig')
+		local lspconfig = require("lspconfig")
 
 		-- npm install -g typescript typescript-language-server
-		require'lspconfig'.tsserver.setup{}
+		require("lspconfig").tsserver.setup{}
 
 		-- go install golang.org/x/tools/gopls@latest
-		-- require'lspconfig'.gopls.setup{}
+		-- require("lspconfig").gopls.setup{}
 
 		-- composer global require vimeo/psalm
-		-- require'lspconfig'.psalm.setup{}
+		-- require("lspconfig").psalm.setup{}
 	end
 }
