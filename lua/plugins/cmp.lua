9eb0cea40758f2efb418640e4980178e7b035e6e
Marton Kaufmann <kaufmann.marton@protonmail.com>
2023-11-17 12:55:24 +0100

    Remove vssnip add signature helper

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -4,10 +4,8 @@ return {
 		"hrsh7th/cmp-nvim-lsp", 
 		"hrsh7th/cmp-buffer",
 		"hrsh7th/cmp-path",
-		"hrsh7th/cmp-path",
 		"hrsh7th/nvim-cmp",
-		"hrsh7th/cmp-vsnip",
-		"hrsh7th/vim-vsnip"
+        "hrsh7th/cmp-nvim-lsp-signature-help"
 	},
 	config = function()
 		local cmp = require("cmp")
@@ -31,7 +29,7 @@ return {
 			}),
 			sources = cmp.config.sources({
 				{ name = "nvim_lsp" },
-				{ name = "vsnip" },
+                { name = "nvim_lsp_signature_help" },
 			}, {
 				{ name = "buffer" },
 			})
