b851ab85616ea2f81a335f63185b5bcb143f67ae
Marton Kaufmann <kaufmann.marton@protonmail.com>
2025-03-17 13:12:27 +0100

    Disable all lsps by default

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -5,20 +5,23 @@ return {
 		local lspconfig = require("lspconfig")
 
 		-- npm install -g typescript typescript-language-server
-		lspconfig.ts_ls.setup{
-            single_file_support = false
-        }
+		-- lspconfig.ts_ls.setup{
+		--     single_file_support = false
+		-- }
 
         -- npm install -g @biomejs/biome
-		lspconfig.biome.setup{}
+		-- lspconfig.biome.setup{}
+
+        -- npm i -g vscode-langservers-extracted
+		-- lspconfig.eslint.setup{}
 
 		-- go install golang.org/x/tools/gopls@latest
-		lspconfig.gopls.setup{}
+		-- lspconfig.gopls.setup{}
 
-        lspconfig.gleam.setup{}
+        -- lspconfig.gleam.setup{}
 
         -- npm install -g intelephense
         -- create license file intelephense/licence.txt
-        lspconfig.intelephense.setup{}
+        -- lspconfig.intelephense.setup{}
 	end
 }
