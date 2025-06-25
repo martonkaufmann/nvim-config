09218228d277181d9cb9da2dcea49aca14a04963
Marton Kaufmann <kaufmann.marton@protonmail.com>
2025-06-25 05:24:55 +0200

    Update lsp config

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -2,26 +2,24 @@ return {
 	"neovim/nvim-lspconfig",
     event = "VeryLazy",
 	config = function()
-		local lspconfig = require("lspconfig")
-
 		-- npm install -g typescript typescript-language-server
-		lspconfig.ts_ls.setup{
-		    single_file_support = false
-		}
+        vim.lsp.enable("ts_ls")
 
         -- npm install -g @biomejs/biome
-		lspconfig.biome.setup{}
+        vim.lsp.enable("biome")
 
         -- npm i -g vscode-langservers-extracted
-		lspconfig.eslint.setup{}
+        vim.lsp.enable("eslint")
+        vim.lsp.enable("jsonls")
 
 		-- go install golang.org/x/tools/gopls@latest
-		lspconfig.gopls.setup{}
+        vim.lsp.enable("gopls")
 
         -- lspconfig.gleam.setup{}
 
         -- npm install -g intelephense
         -- create license file intelephense/licence.txt
-        -- lspconfig.intelephense.setup{}
+        -- vim.lsp.enable("intelephense")
+        -- vim.lsp.enable("phpactor")
 	end
 }
