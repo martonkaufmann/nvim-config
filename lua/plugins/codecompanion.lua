07dbdd18170effca9616af0126b0e4e9f34c510d
Marton Kaufmann <kaufmann.marton@protonmail.com>
2025-10-18 10:10:38 +0200

    Use copilot instead of claude

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -1,38 +1,55 @@
 return {
-  "olimorris/codecompanion.nvim",
-  event = "VeryLazy",
-  opts = {},
-  config = function()
-    require("codecompanion").setup({
-      strategies = {
-        chat = {
-          adapter = "anthropic",
+    "olimorris/codecompanion.nvim",
+    event = "VeryLazy",
+    opts = {},
+    config = function()
+        require("codecompanion").setup({
+            strategies = {
+                chat = {
+                    adapter = "copilot",
+                },
+                inline = {
+                    adapter = "copilot",
+                },
+            },
+            memory = {
+                opts = {
+                    chat = {
+                        enabled = true,
+                    },
+                },
+                default = {
+                    description = "Collection of common files for all projects",
+                    files = {
+                        "AGENT.md",
+                        "AGENTS.md",
+                        "instructions",
+                    },
+                },
+            },
+        })
+    end,
+    dependencies = {
+        "github/copilot.vim",
+        "nvim-lua/plenary.nvim",
+        "nvim-treesitter/nvim-treesitter",
+        {
+            "MeanderingProgrammer/render-markdown.nvim",
+            ft = { "markdown", "codecompanion" },
+            latext = { enabled = false },
         },
-        inline = {
-          adapter = "anthropic",
+        {
+            "HakonHarnes/img-clip.nvim",
+            opts = {
+                filetypes = {
+                    codecompanion = {
+                        prompt_for_file_name = false,
+                        template = "[Image]($FILE_PATH)",
+                        use_absolute_path = true,
+                    },
+                },
+            },
         },
-      },
-    })
-  end,
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-treesitter/nvim-treesitter",
-    {
-      "MeanderingProgrammer/render-markdown.nvim",
-      ft = { "markdown", "codecompanion" },
-      latext = { enabled = false },
     },
-    {
-      "HakonHarnes/img-clip.nvim",
-      opts = {
-        filetypes = {
-          codecompanion = {
-            prompt_for_file_name = false,
-            template = "[Image]($FILE_PATH)",
-            use_absolute_path = true,
-          },
-        },
-      },
-    },
-  },
 }
+
